6515c82a525b7430852e82552b412bddf50f5e5c
root <root@ip-172-31-18-62.ap-southeast-1.compute.internal>
2022-08-08 06:15:17 +0000

    deploy

---
+++ precache-manifest.57830207ca959628714defc128856804.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111aec1e5484b5b28fa2bedd1f0a24a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a96d0ba187899d2912fbefbd0b052bb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "e2128cd22f56d777dc89",
+    "url": "/static/css/Chat.ce3b01f5.css"
+  },
+  {
+    "revision": "0a1dcfb8b59c7e28bcbf",
+    "url": "/static/css/NftMarketMain.d38991ea.css"
+  },
+  {
+    "revision": "aaaa7bdc52442513041d",
+    "url": "/static/css/Rank.486efe42.css"
+  },
+  {
+    "revision": "87e7951b1b1648626c8e",
+    "url": "/static/css/app.b0e98270.css"
+  },
+  {
+    "revision": "628a9f6c528b9b77db1b",
+    "url": "/static/css/chunk-libs.dba3830b.css"
+  },
+  {
+    "revision": "13b5278bf711a7393793",
+    "url": "/static/css/chunk-vant.1223d0f0.css"
+  },
+  {
+    "revision": "60831a2d463829f32b157e5eccc30102",
+    "url": "/static/fonts/iconfont.60831a2d.woff"
+  },
+  {
+    "revision": "c07f14b5c1eb0c0cf21c9db53b0b3e8b",
+    "url": "/static/fonts/iconfont.c07f14b5.woff2"
+  },
+  {
+    "revision": "da531675ca88879dadaf265b8b725c90",
+    "url": "/static/fonts/iconfont.da531675.eot"
+  },
+  {
+    "revision": "f5d9e480d97d1142fc30e3aec9f18151",
+    "url": "/static/fonts/iconfont.f5d9e480.ttf"
+  },
+  {
+    "revision": "0415cf4ebd3976a0db17e2fa1e9812f6",
+    "url": "/static/fonts/index.0415cf4e.eot"
+  },
+  {
+    "revision": "87ee1a70c3af3cb1a4d8de1d96568fa0",
+    "url": "/static/fonts/index.87ee1a70.woff"
+  },
+  {
+    "revision": "abed1cb7ce7794b656c39076aaca6970",
+    "url": "/static/fonts/index.abed1cb7.woff2"
+  },
+  {
+    "revision": "de94a85c73334510b8645762a276917d",
+    "url": "/static/fonts/index.de94a85c.ttf"
+  },
+  {
+    "revision": "ecfdc67661b8cdfc4a022ffa5cd3ab5d",
+    "url": "/static/img/empty-noBalance-light.ecfdc676.svg"
+  },
+  {
+    "revision": "c45d86af74a4117196598a7ec02942f5",
+    "url": "/static/img/empty-noData-dark.c45d86af.svg"
+  },
+  {
+    "revision": "9816939cb2282fbf2c068b9a19dffb4f",
+    "url": "/static/img/empty-noData-light.9816939c.svg"
+  },
+  {
+    "revision": "f79c66574cf08d6f342a7a6d3be55553",
+    "url": "/static/img/empty-noSearch-dark.f79c6657.svg"
+  },
+  {
+    "revision": "7e616a93755018473ab4f7610a1e6269",
+    "url": "/static/img/empty-noSearch-light.7e616a93.svg"
+  },
+  {
+    "revision": "75f6e9a0019cdd5252d2925b4a9acd52",
+    "url": "/static/img/icon-default.75f6e9a0.png"
+  },
+  {
+    "revision": "229c6d94b58cc98c0aa752bccaa92400",
+    "url": "/static/img/iconfont.229c6d94.svg"
+  },
+  {
+    "revision": "5d7b85c12e27a0b3f576725c7556d69d",
+    "url": "/static/img/index.5d7b85c1.svg"
+  },
+  {
+    "revision": "89d32a3d882930d615953863c743a4a4",
+    "url": "/static/img/new.89d32a3d.png"
+  },
+  {
+    "revision": "e2128cd22f56d777dc89",
+    "url": "/static/js/Chat.6f717cf3.js"
+  },
+  {
+    "revision": "e6994015fa0b2143a1b8",
+    "url": "/static/js/Lost.a7641347.js"
+  },
+  {
+    "revision": "e18f1c1949a0d97ebbdd",
+    "url": "/static/js/NftMarketIndex.4a5cc644.js"
+  },
+  {
+    "revision": "0a1dcfb8b59c7e28bcbf",
+    "url": "/static/js/NftMarketMain.44f8869f.js"
+  },
+  {
+    "revision": "aaaa7bdc52442513041d",
+    "url": "/static/js/Rank.62563215.js"
+  },
+  {
+    "revision": "87e7951b1b1648626c8e",
+    "url": "/static/js/app.3cafb93f.js"
+  },
+  {
+    "revision": "62a55bf5b27aa2531cc4",
+    "url": "/static/js/chunk-echarts.88503182.js"
+  },
+  {
+    "revision": "628a9f6c528b9b77db1b",
+    "url": "/static/js/chunk-libs.37c900c4.js"
+  },
+  {
+    "revision": "13b5278bf711a7393793",
+    "url": "/static/js/chunk-vant.452b8cc0.js"
+  },
+  {
+    "revision": "a3cc0ec1ff14bcd7acad",
+    "url": "/static/js/runtime.a12ebd19.js"
+  },
+  {
+    "revision": "353ccab10bf51705d431",
+    "url": "/static/js/vendors~Chat.2fc602a7.js"
+  }
+]);
